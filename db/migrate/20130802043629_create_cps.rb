b3d9c801974120d91c0a92230ff5ae4dc2f5a029
Tianxin Dai <tianxindai@Tainxims-MacBook-Pro.local>
2013-08-03 15:28:58 -0700

    added character and cp model

--- db/migrate/20130802043629_create_cps.rb
+++ db/migrate/20130802043629_create_cps.rb
@@ -2,9 +2,12 @@ class CreateCps < ActiveRecord::Migration
   def change
     create_table :cps do |t|
       t.integer :creator, :null => false
-      t.datetime, :created_at
-      t.integer :character1_id
-      t.integer :character2_id
+      t.datetime :created_at
+      t.integer :character1_id, :null => false
+      t.integer :character2_id, :null => false
+      t.integer :category
+      t.text :summary
+      t.text :wiki_content
     end
   end
 end
