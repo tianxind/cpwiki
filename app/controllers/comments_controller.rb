b716dc1cf1cc717add7a97763110791e1d092cad
Kain <kainqiu@gmail.com>
2013-08-12 21:38:15 -0700

    comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,41 @@
+class CommentsController < ApplicationController
+   before_filter :authenticate_user!
+   # skip_before_filter :verify_authenticity_token, :only => [:create]
+
+  def new
+    @comment = Comment.new
+  end
+  
+  def create
+    p ">>>>>>>>> in comment create"
+    p "new comment is "
+    p params[:comment_text]
+    p "in create comment id is >>>>>>>>>>"
+    p params[:id]
+    p "current user id is >>>>>>>>"
+    p current_user.id.to_s
+    @comment = Comment.new(:cp_id => params[:id],
+                           :user_id => current_user.id,
+                           :comment_text => params[:comment_text],
+                           :date_time => Time.now)
+    if !@comment.save
+      p ">>>>>>>>> comment saving failed"
+    else 
+      p ">>>>>>>>> save succeed!"
+      redirect_to :controller => :cps, :action => :show, :id => params[:id]
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
