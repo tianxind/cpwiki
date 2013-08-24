9d8a75e49e977ff5b3b69fc4cb55d4153f57f7bb
Kain <kainqiu@gmail.com>
2013-08-24 16:10:15 -0700

    fix empty comment

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,5 @@
+#!/bin/env ruby
+# encoding: utf-8
 class Like < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :user
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
+#!/bin/env ruby
+# encoding: utf-8
 class Photo < ActiveRecord::Base
   belongs_to :user
   
