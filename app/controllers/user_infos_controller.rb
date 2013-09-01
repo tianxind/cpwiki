7317a62992e3b35205cc352e7f3ac33a0f725c11
Tianxin Dai <tianxin@tianxin-macbookpro.roam.corp.google.com>
2013-09-01 16:54:34 -0700

    before pulling kain's changes

--- app/controllers/user_infos_controller.rb
+++ app/controllers/user_infos_controller.rb
@@ -7,16 +7,9 @@ class UserInfosController < ApplicationController
   
   def index
     @user = User.find_by_id(current_user.id)
-    #p "user info is "
-    #p @user
 
     @cp_entries = Cp.find_all_by_creator_id(@user.id)
-    #@cp_entries = @user.cps
-    #p "all entries created by user is"
-    #p @cp_entries
 
     @user_likes = Like.find_all_by_user_id(@user.id)
-    #p "all likes by user is"
-    #p @user_likes
   end
 end
