e9b383938cd98964a71626a0d7db66220af10ea0
Kain <kainqiu@gmail.com>
2013-09-28 14:33:02 -0700

    Ignore .elasticbeanstalk from Git and fix ol li

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # # custom 404
+  # unless Rails.application.config.consider_all_requests_local
+  #   rescue_from ActiveRecord::RecordNotFound,
+  #               ActionController::RoutingError,
+  #               ActionController::UnknownController,
+  #               ActionController::UnknownAction,
+  #               ActionController::MethodNotAllowed do |exception|
+
+  #     # Put loggers here, if desired.
+
+  #     redirect_to :controller => :home, :action => :index
+  #   end
+  # end
+
 end
