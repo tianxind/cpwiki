3819d5a1bc82d0945c121d2d0679179cf671e30d
Tianxin Dai <tianxin@tianxin-macbookpro.roam.corp.google.com>
2013-09-28 18:03:12 -0700

    fixed comment button problem

--- app/assets/javascripts/imageComment.js
+++ app/assets/javascripts/imageComment.js
@@ -1,40 +1,11 @@
 // Add comment button to all images in wiki content
 function addCommentButtonToImages() {
 	var images = $('.wiki_image');
-	console.log(images);
 	var addCommentButton = $('#add_comment');
 	var hoveredImage = false;
-	images.mouseenter(function(e) {
-		// console.log("show add comment button");
-		hoveredImage = this;
-		var offset = $(this).offset();
-		var top = offset.top - $('.right_container').offset().top;
-		var left = offset.left - $('.left_sidebar').width() - $('.homepage_content').css('marginLeft');
-		addCommentButton.show();
-		addCommentButton.css({
-			top: top + 'px',
-			left: left + 'px'
-		});
-	});
-	images.mouseleave(function(e) {
-		if (e.relatedTarget === addCommentButton) {
-	    	return;
-		}
-		addCommentButton.hide();
-		hoveredImage = false;
-	});
-	addCommentButton.mouseleave(function(e){
-		if (e.relatedTarget === hoveredImage) {
-	    	return;	
-		}
-		addCommentButton.hide();
-		hoveredImage = false;
-	});
-
 	var len = images.length;
 	for (var i = 0; i < len; ++i) {
 		console.log($(images[i]).attr('src'));
-		// var link = $("<a href='/photos/lookup?src='" + escape($(images[i]).attr('src')) + "'></a>");
 		var imageSrc = $(images[i]).attr('src');
 		if (imageSrc.indexOf("/images/") == 0) {
 			imageSrc = imageSrc.replace("/images/", "");
@@ -47,4 +18,30 @@ function addCommentButtonToImages() {
 		console.log(link);
 		$(images[i]).replaceWith(link);
 	}
+
+    var images = $('.wiki_image')
+    images.mouseenter(function(e) {
+		console.log("show add comment button");
+		console.log($(this).height());
+		console.log($(this).width());
+		hoveredImage = this;
+		var offset = $(this).offset();
+		var top = offset.top - $('.right_container').offset().top;
+		homepageContentMarginLeft = parseInt($('.homepage_content').css('marginLeft').split("px")[0]);
+		var left = offset.left - $('.left_sidebar').width() - homepageContentMarginLeft;
+		addCommentButton.show();
+		addCommentButton.css({
+			top: top + 'px',
+			left: left + 'px'
+		});
+		// console.log("addCommentButton left:" + addCommentButton.css('left'));
+	});
+	images.mouseleave(function(e) {
+	    console.log("hide add comment button");
+		if (e.relatedTarget === addCommentButton) {
+	      return;
+		}
+		addCommentButton.hide();
+		hoveredImage = false;
+	});
 }
